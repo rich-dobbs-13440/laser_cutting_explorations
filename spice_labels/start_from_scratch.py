05df07328fd7430fd8b254da89e7eb9dea5c3f53
Rich Dobbs <rich.dobbs.13440@gmail.com>
2023-06-05 12:24:26 -0600

    Got it to parse include statements

--- spice_labels/start_from_scratch.py
+++ spice_labels/start_from_scratch.py
@@ -1,5 +1,6 @@
 import ply.lex as lex
 import ply.yacc as yacc
+import re
 
 
 precedence = (
@@ -13,17 +14,21 @@ precedence = (
 )
 
 
+
 # Token list
 tokens = (
     'NUMBER',
     'ID',
-    'ASSIGN',
+    'EQUALS',
     'LPAREN',
     'RPAREN',
     'LBRACE',
     'RBRACE',
     'SEMICOLON', 
-    'COMMENT',   
+    'COMMENT', 
+    'BLOCKCOMMENT', 
+    'INCLUDE_STATEMENT', 
+    'USE_STATEMENT',
     'COMMA', 
     'PLUS',
     'MINUS',
@@ -42,9 +47,51 @@ tokens = (
     'NOT',          
 )
 
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+# Ignored characters
+t_ignore = ' \t'  
+
 
 # Token regex definitions
-t_ASSIGN = r'='
+t_BLOCKCOMMENT = r'/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/'
+
+
+
+# Line comment handling
+def t_COMMENT(t):
+    r'\/\/.*'
+    pass
+
+def t_INCLUDE_STATEMENT(t):
+    r'include\s+<[^*?<>]*>'
+    match = re.match(r'include\s+<([^*?<>]*)>', t.value)
+    t.value = match.groups(0)[0] 
+    return t
+
+
+def t_USE_STATEMENT(t):
+    r'use\s+<[^*?<>]*>'
+    match = re.match(r'use\s+<([^*?<>]*)>', t.value)
+    t.value = match.groups(0)[0] 
+    return t
+
+
+# Token handlers
+def t_NUMBER(t):
+    r'[-+]?\d+(\.\d+)?([eE][-+]?\d+)?'
+    return t
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    return t
+
+
+
+
+t_EQUALS = r'='
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
 t_LBRACE = r'\{'
@@ -67,27 +114,7 @@ t_AND = r'&&'
 t_OR = r'\|\|'
 t_NOT = r'!'
 
-# Ignored characters
-t_ignore = ' \t'
-
-
-# Token handlers
-def t_NUMBER(t):
-    r'[-+]?\d+(\.\d+)?([eE][-+]?\d+)?'
-    return t
-
-def t_ID(t):
-    r'[a-zA-Z_][a-zA-Z0-9_]*'
-    return t
-
-# Line comment handling
-def t_COMMENT(t):
-    r'\/\/.*'
-    pass
-
-def t_newline(t):
-    r'\n+'
-    t.lexer.lineno += len(t.value)
+  
 
 # Error handler
 def t_error(t):
@@ -96,22 +123,96 @@ def t_error(t):
 
 # Grammar rules
 
-def p_start_empty(p):
-    'start : empty'
-    pass
-
-def p_start_statement(p):
-    'start : assignment_statement'
-    pass
-
-def p_start_action_statement(p):
-    'start : action_statement'
-    pass
-
+def p_start(p):
+    '''start : empty
+             | assignment_statement
+             | action_statement
+             | line_comment
+             | block_comment             
+             | module_definition
+             | function_definition
+             | include_statement
+             | use_statement'''
+    p[0] = p[1]
 
 def p_empty(p):
     'empty :'
     pass
+
+
+def p_statement_assign(p):
+    'assignment_statement : ID EQUALS expression SEMICOLON'
+    symbol = p[1]
+    value = p[3]
+    symbol_table[symbol] = value
+    print("Assignment: {} = {}".format(symbol, value)) 
+
+def p_action_statement(p):
+    'action_statement : ID LPAREN parameter_values RPAREN action_end'
+    module_name = p[1]
+    parameter_values = p[3]
+    print("Action statement: {}({})".format(module_name, parameter_values))    
+
+
+def p_line_comment(p):
+    '''line_comment : COMMENT'''
+    pass 
+
+def p_block_comment(p):
+    '''block_comment : BLOCKCOMMENT'''
+    # Your logic to handle block comments goes here
+    p[0] = p[1]  # Store the block comment text in p[0]
+
+def p_module_definition(p):
+    '''module_definition : ID parameter_list LBRACE RBRACE'''
+    pass
+
+def p_function_definition(p):
+    'function_definition : ID parameter_list EQUALS expression SEMICOLON'
+    # Your logic to handle function definition goes here
+    print("Function definition:", p[1], p[3], "=", p[6])
+
+def p_include_statement(p):
+    'include_statement : INCLUDE_STATEMENT'
+    filepath = p[1]
+    # Handle the include statement here
+    print("Include: {}".format(filepath))
+
+def p_use_statement(p):
+    'use_statement : USE_STATEMENT'
+    filepath = p[1]
+    # Handle the use statement here
+    print("Use: {}".format(filepath))
+
+def p_parameter_list(p):
+    '''parameter_list : LPAREN parameter_item_list RPAREN
+                      | LPAREN RPAREN'''
+    # Your logic to handle parameter list goes here
+    if len(p) == 4:
+        p[0] = p[2]  # If parameters are present, assign them to p[0]
+    else:
+        p[0] = []  # Empty parameter list
+
+def p_parameter_item_list(p):
+    '''parameter_item_list : parameter_item
+                           | parameter_item_list COMMA parameter_item'''
+    # Your logic to handle multiple parameter items goes here
+    if len(p) == 2:
+        p[0] = [p[1]]  # Single parameter item
+    else:
+        p[0] = p[1] + [p[3]]  # Concatenate parameter items
+
+def p_parameter_item(p):
+    '''parameter_item : ID
+                      | ID EQUALS expression'''
+    # Your logic to handle a parameter item goes here
+    if len(p) == 2:
+        p[0] = (p[1], None)  # Parameter without default value
+    else:
+        p[0] = (p[1], p[3])  # Parameter with default value
+
+
+
 
 def p_expression_binop(p):
     '''expression : expression PLUS expression
@@ -151,44 +252,25 @@ def p_expression(p):
         pass
     else:  # Symbolic expression
         p[0] = p[1]
-        # symbol = p[1]
-        # if symbol in symbol_table:
-        #     p[0] = symbol_table[symbol]
-        # else:
-        #     print("Error: Symbol '{}' is undefined".format(symbol))
-        #     p[0] = None
 
-def p_statement_assign(p):
-    'assignment_statement : ID ASSIGN expression SEMICOLON'
-    symbol = p[1]
-    value = p[3]
-    symbol_table[symbol] = value
-    print("Assignment: {} = {}".format(symbol, value)) 
+ 
+
+def p_parameter_values(p):
+    '''parameter_values : expression
+                        | expression COMMA parameter_values'''
+    if len(p) == 2:
+        p[0] = [p[1]]
+    else:
+        p[0] = [p[1]] + p[3]
+
+def p_action_end_semicolon(p):
+    'action_end : SEMICOLON'
+    pass
 
 
-# def p_action_statement_module_call(p):
-#     'action_statement : ID LPAREN parameter_values RPAREN action_end'
-#     module_name = p[1]
-#     parameters = p[3]
-#     print("Module Call: {}({})".format(module_name, parameters))
-
-# def p_parameter_values(p):
-#     '''parameter_values : expression
-#                         | expression COMMA parameter_values'''
-#     if len(p) == 2:
-#         p[0] = [p[1]]
-#     else:
-#         p[0] = [p[1]] + p[3]
-
-# def p_parameter_values_single(p):
-#     'parameter_values : parameter_value'
-#     p[0] = [p[1]]
-
-# def p_parameter_value_with_name(p):
-#     'parameter_value : ID ASSIGN expression'
-#     parameter_name = p[1]
-#     parameter_value = p[3]
-#     p[0] = (parameter_name, parameter_value)
+def p_action_end_block(p):
+    'action_end : LBRACE start RBRACE'
+    pass        
 
 
 def p_error(p):
@@ -218,13 +300,25 @@ def process_input(input_text):
     print(parser.parse(input_text, lexer=lexer))
 
 
-process_input("")
-process_input("""
-     a = 12; 
-     b = 13;
-     c = a;
-     d = a + 12;
-     can(d=12, h=13);
+# # Empty 
+# process_input("")
+# # Single numeric assignment
+# process_input("""
+#      a = 12; 
+#     """)
 
-    """)
+
+# Include statement syntax
+process_input("include <ScadStoicheia/centerable.scad>");
+process_input("
+use <ScadStoicheia/visualization.scad>
+# # Multiple numeric assignment
+# process_input("""
+#      a = 12; 
+#      b = 13;
+#     """)
+#     #  "
+#     #  can(d, h);
+
+#     # """)
               
